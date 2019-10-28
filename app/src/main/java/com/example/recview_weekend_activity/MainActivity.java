d3e8ca93371167d9d1e88dee5b82fc3065132db4
consultant <derrick.davis@mobileappscompany.com>
2019-10-28 18:46:03 -0400

    first commit on new machine

---
+++ app/src/main/java/com/example/recview_weekend_activity/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.recview_weekend_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements RecviewAdapter.ItemClickListener {
+    RecviewAdapter adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList<String> titleNames = new ArrayList<>();
+        titleNames.add("To Infintiy and Beyond");
+        titleNames.add("Soul food on a Plane");
+        titleNames.add("Heads Up 7 Up");
+        titleNames.add("RedRum");
+        titleNames.add("Gilligan's Planet");
+        titleNames.add("To the Beyond");
+        titleNames.add("Over Under the Hill");
+        titleNames.add("To and From");
+        titleNames.add("Beyond and Back");
+        titleNames.add("Finish first or last");
+        titleNames.add("To Be or ...... Not");
+        titleNames.add("Can one Trust Another");
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        recyclerView.setHasFixedSize(true);
+
+        /*DividerItemDecoration dividerItemDecoration = new DividerItemDecoration
+                (recyclerView.getContext(),layoutManager.getOrientation());
+                recyclerView.addItemDecoration(dividerItemDecoration);*/
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new RecviewAdapter(this,titleNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+    }
+    @Override
+    public void onItemClick(View view, int position){
+        Toast.makeText(this, "You clicked" + adapter.getItem(position) + "on row number" + position, Toast.LENGTH_SHORT).show();
+
+    }
+}
---
+++ app/src/main/java/com/example/recview_weekend_activity/RecviewAdapter.java
@@ -0,0 +1,77 @@
+package com.example.recview_weekend_activity;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecviewAdapter extends RecyclerView.Adapter<RecviewAdapter.ViewHolder> {
+
+    private List<String> mTitleInfo;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+
+    RecviewAdapter(Context context, List<String> tleInfo){
+     this.mInflater = LayoutInflater.from(context);
+     this.mTitleInfo = tleInfo;
+    }
+
+    @NonNull
+    @Override
+    public RecviewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = mInflater.inflate(R.layout.recviewitem,parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecviewAdapter.ViewHolder holder, int position) {
+
+        String titlview = mTitleInfo.get(position);
+        holder.tvtle.setText(titlview);
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return mTitleInfo.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView tvtle;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            tvtle = itemView.findViewById(R.id.tv_title);
+            itemView.setOnClickListener(this);
+        }
+    @Override
+
+        public  void onClick(View view) {
+         if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+            }
+        }
+
+
+        String getItem(int id){
+            return mTitleInfo.get(id);
+
+        }
+        void setClickListener(ItemClickListener itemClickListener){
+            this.mClickListener = itemClickListener;
+        }
+
+
+        public interface ItemClickListener{
+        void onItemClick (View view, int position);
+
+            }
+}
